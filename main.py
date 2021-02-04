52ebf70705cb87fe090393a19be201b44e210feb
Siqi <mosiqi1996@hotmail.com>
2021-02-04 21:05:01 +0000

    add project acafflod

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import jsonify
+import pandas as pd
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return 'Hello World! Continuous delivery.  This is auto-deploy.  Take 10'
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"This was placed in the url: new-{name}")
+    val = {"new-name": name}
+    return jsonify(val)
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
